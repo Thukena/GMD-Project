f30ca28ef1c59f70d99d6bf43fef312bef0acbd9
Marcus de Place <marcus6001@live.dk>
2024-05-05 18:06:26 +0200

    EvilSamu now jumps when colliding with a wall

--- Assets/Scripts/Enemies/DirectFollow.cs
+++ Assets/Scripts/Enemies/DirectFollow.cs
@@ -1,4 +1,5 @@
 using Player;
+using Shared.Collision;
 using UnityEngine;
 
 namespace Enemies
@@ -6,12 +7,19 @@ namespace Enemies
     public class DirectFollow : MonoBehaviour, IFollow
     {
         [SerializeField] private PlayerMovement playerMovement;
+        [SerializeField] private WallChecker wallChecker;
 
         public bool IsFollowing { get; set; }
 
         public void FollowTarget(Transform target)
         {
             IsFollowing = true;
+            
+            if (wallChecker.isTouchingWall)
+            {
+                playerMovement.Jump();
+            }
+            
             playerMovement.Move(target.position.x > transform.position.x ? 1 : -1);
         }
 
