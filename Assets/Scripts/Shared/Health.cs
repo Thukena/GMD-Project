0d1c67456b97b9ffc16ba3ab0e895a796876bde3
Marcus de Place <marcus6001@live.dk>
2024-05-21 20:45:41 +0200

    Added healthBar for enemies and the Player

--- Assets/Scripts/Shared/Health.cs
+++ Assets/Scripts/Shared/Health.cs
@@ -1,22 +1,25 @@
-
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Shared
 {
     public class Health : MonoBehaviour
     {
         [SerializeField] private int maxHealth;
+        [SerializeField] private Image greenHealthBar;
         private int currentHealth;
 
         private void Start()
         {
             currentHealth = maxHealth;
+            greenHealthBar.fillAmount = 1f;
         }
-        
+
         public void TakeDamage(int damage)
         {
-            print("DAMAGE TAKEN: " + damage);
             currentHealth -= damage;
+            greenHealthBar.fillAmount = (float)currentHealth / maxHealth;
+            
             if (currentHealth <= 0)
             {
                 print("DEAD");
