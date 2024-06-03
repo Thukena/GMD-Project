e736590d96e8e02cec747315aa552aec2cddf3c2
Marcus de Place <marcus6001@live.dk>
2024-06-04 00:02:36 +0200

    Now spawns random enemyPrefab and placement

--- Assets/Scripts/GameManagement/EnemySpawner.cs
+++ Assets/Scripts/GameManagement/EnemySpawner.cs
@@ -1,3 +1,4 @@
+using Player;
 using Shared;
 using UnityEngine;
 
@@ -7,18 +8,36 @@ namespace GameManagement
     {
         [SerializeField] private GameObject EvilSamuPrefab;
         [SerializeField] private GameObject MoodSwingerPrefab;
-        [SerializeField] private float spawnInterval = 10.0f; 
-
+        [SerializeField] private float spawnInterval;
+        [SerializeField] private float spawnDistance;
+        private bool _spawnOnRightSide;
+        private Transform playerTransform;
         private void Start()
         {
+            playerTransform = PlayerController.Instance.transform;
             InvokeRepeating("SpawnEnemy", 0.0f, spawnInterval);
         }
 
         private void SpawnEnemy()
         {
-            var enemy = Instantiate(EvilSamuPrefab, new Vector3(0, 10, 0), Quaternion.identity);
+            var random = Random.Range(0f, 100f);
+
+            GameObject chosenPrefab;
+            
+            if (random < 50f)
+            {
+                chosenPrefab = EvilSamuPrefab;
+            }
+            else
+            {
+                chosenPrefab = MoodSwingerPrefab;
+            }
+
+            var spawnArea = new Vector2(playerTransform.position.x + (_spawnOnRightSide ? 1 : -1) * spawnDistance, playerTransform.position.y + 10);
+            var enemy = Instantiate(chosenPrefab, spawnArea, Quaternion.identity);
+            _spawnOnRightSide = !_spawnOnRightSide;
             var health = enemy.GetComponent<Health>();
-            health.OnDeath += () => GameManager.Instance.OnEnemyDeath(50);
+            health.OnDeath += () => GameManager.Instance.OnEnemyDeath(34);
         }
     }
 }
