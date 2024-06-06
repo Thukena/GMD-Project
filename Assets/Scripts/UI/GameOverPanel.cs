291fe91e2f4db49188c3fb1dc4e461971d959eb1
Marcus de Place <marcus6001@live.dk>
2024-06-06 13:09:52 +0200

    Game over now displays stage number

--- Assets/Scripts/UI/GameOverPanel.cs
+++ Assets/Scripts/UI/GameOverPanel.cs
@@ -7,10 +7,12 @@ namespace UI
     public class GameOverPanel : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI timerText;
+        [SerializeField] private TextMeshProUGUI stageText;
         [SerializeField] private TimeHandler timeHandler;
         private void OnEnable()
         {
-            timerText.text = $"{timeHandler} Minutes!";
+            timerText.text = $"{timeHandler} Minutes";
+            stageText.text = $"And made it to Stage {SceneController.Instance.currentStage}!";
         }
 
         public void RestartGame()
